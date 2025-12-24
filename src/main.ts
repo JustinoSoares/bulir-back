b8f65dff7bdef5a29efeb6192c2d8337910c2412
Justino Soares <justinocsoares123@gmail.com>
2025-12-24 20:29:42 +0100

    resolvendo erro de cors

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
-
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3001',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, // remove propriedades não declaradas no DTO
     forbidNonWhitelisted: true, // lança erro se houver propriedades não declaradas
     transform: true, // transforma payloads para os tipos declarados nos DTOs
-    
-    // customiza a mensagem de erro para o primeiro erro encontrado
+
     exceptionFactory: (errors) => {
       const firstError = errors[0];
       const FirstMessage = firstError ? Object.values(firstError.constraints!)[0] : 'Validation error';
